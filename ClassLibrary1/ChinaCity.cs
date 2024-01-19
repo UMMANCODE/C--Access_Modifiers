2a7bb6025a2a4c00d9a959ecab29c8046b02ed6c
ummanmemmedov2005@gmail.com <ummanmm@code.edu.az>
2024-01-19 16:25:37 +0400

    Files added

---
+++ ClassLibrary1/ChinaCity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1 {
+    public class ChinaCity : City {
+        public override int Population {
+            set {
+                if (value >= 100000) _population = value;
+            }
+        }
+    }
+}
---
+++ ClassLibrary1/City.cs
@@ -0,0 +1,37 @@
+﻿namespace ClassLibrary1 {
+    public abstract class City {
+        protected string _name;
+        protected int _population;
+
+        public string Name {
+            get {
+                return _name;
+            }
+            set {
+                if (CheckName(value)) _name = value;
+            }
+        }
+
+        public virtual int Population {
+            get {
+                return _population;
+            }
+            set {
+                if (value >= 0) _population = value;
+            }
+        }
+
+        private bool CheckName(string value) {
+            bool isNameValid = true;
+            for (int i = 0; i < value.Length; i++) {
+                if (!char.IsLetter(value[i])) {
+                    isNameValid = false;
+                }
+            }
+            if (value.Length < 3 || value.Length > 20)
+                isNameValid = false;
+            return isNameValid;
+        }
+
+    }
+}
